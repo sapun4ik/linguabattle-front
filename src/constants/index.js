3aa485794a0f08dafc6a93002bad7330ff19495d
Anatoly Sapunov <sapun4ik@gmail.com>
2021-01-08 12:17:32 +0300

    updated base url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = 'http://localhost:8080';
-export const API_BASE_URL = 'http://applinguabattle-env.eba-fgqg2dve.us-east-2.elasticbeanstalk.com';
+export const API_BASE_URL = 'https://www.api.linguabattle.online';
 export const ACCESS_TOKEN = 'accessToken';
 
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
